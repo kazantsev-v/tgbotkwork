4b37c3fba1b8fb322d11f0f427bfe0c28cc2d062
Vitaly <kazans190808@gmail.com>
2025-01-30 20:09:04 +0500

    Update config.js

--- jsbot/config/config.js
+++ jsbot/config/config.js
@@ -2,7 +2,7 @@ config = {
     botToken: process.env.botToken,
     termsForWorkersLink:"https://law.2gis.ru/licensing-agreement",
     termsForCustomersLink:"https://law.2gis.ru/licensing-agreement",
-    port: 3002,
+    port: 3013,
     backendURL: 'http://localhost:3001/api'
 }
 
