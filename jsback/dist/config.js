9a9d5c9c67d1ee0e00adda67f5ebfc7588765ee4
Vitaly <kazans190808@gmail.com>
2025-02-05 23:37:53 +0500

    12121

--- jsback/dist/config.js
+++ jsback/dist/config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const config = {
     botURL: 'http://localhost:3002',
-    self: 'http://localhost:3001',
-    port: 3001
+    self: 'http://localhost:3003',
+    port: 3003
 };
 exports.default = config;
--- jsbot/config/config.js
+++ jsbot/config/config.js
@@ -5,8 +5,8 @@ config = {
     botToken: token,
     termsForWorkersLink:"https://law.2gis.ru/licensing-agreement",
     termsForCustomersLink:"https://law.2gis.ru/licensing-agreement",
-    port: 3013,
-    backendURL: 'http://localhost:3001/api'
+    port: 3002,
+    backendURL: 'http://localhost:3003/api'
 }
 
 module.exports = { config };
