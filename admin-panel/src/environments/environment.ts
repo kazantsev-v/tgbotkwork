379a171c1f6252db40a9a3f90b1686d259b60600
Vitaly <kazans190808@gmail.com>
2025-02-06 00:36:23 +0500

    123

--- admin-panel/src/environments/environment.ts
+++ admin-panel/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     superAdminTelegramId: 966126211,
-    backendURL: 'http://localhost:3001'
+    backendURL: 'http://217.171.146.186:3003'
   };
