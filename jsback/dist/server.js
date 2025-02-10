f7465ca83c94de8a800a8e30356974511a136e99
Vitaly <kazans190808@gmail.com>
2025-02-10 20:35:01 +0500

    https

--- jsback/dist/server.js
+++ jsback/dist/server.js
@@ -4,14 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const http_1 = __importDefault(require("http"));
+const https_1 = __importDefault(require("https"));
 const fs_1 = __importDefault(require("fs"));
 const config_1 = __importDefault(require("./config"));
 const PORT = config_1.default.port;
-
-const httpsServer = http_1.default.createServer(app_1.default);
+const privateKey = fs_1.default.readFileSync('../privkey.pem', 'utf8');
+const certificate = fs_1.default.readFileSync('../cert.pem', 'utf8');
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+};
+const httpsServer = https_1.default.createServer(credentials, app_1.default);
 httpsServer.listen(PORT, () => {
     console.log(`Server is running on ${config_1.default.self}`);
 });
