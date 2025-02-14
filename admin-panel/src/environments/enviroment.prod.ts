ff2fd3ab3646e8b77e89ad477b6e81091aa76fa7
Vitaly <kazans190808@gmail.com>
2025-02-14 19:40:30 +0500

    132

--- admin-panel/src/environments/enviroment.prod.ts
+++ admin-panel/src/environments/enviroment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     superAdminTelegramId: 966126211,
-    backendURL: 'http://217.171.146.186:3003'
+    backendURL: 'https://bot.moverspb.ru:3003'
   };
