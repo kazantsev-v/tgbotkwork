9e7b341688751e2ec7ba8e00d7f041de871461ba
Vitaly <kazans190808@gmail.com>
2025-04-22 21:43:40 +0500

    Update server.js

--- stats-page/server.js
+++ stats-page/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3001; // Убедитесь, что этот порт не занят вашим основным бэкендом
+const port = 3005; // Убедитесь, что этот порт не занят вашим основным бэкендом
 
 // Отдаем статические файлы из папки 'public'
 app.use(express.static(path.join(__dirname, 'public')));
